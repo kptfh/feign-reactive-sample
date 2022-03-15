054b3d8b7f99bb4b835e49f84c3daf3845c7ac6d
Sergii Karpenko <skarpenko@playtika.com>
2022-03-15 12:45:44 +0200

    fix blocking clients

--- feign/src/main/java/sample/GreetingReactiveWOtherName.java
+++ feign/src/main/java/sample/GreetingReactiveWOtherName.java
@@ -1,11 +1,14 @@
 package sample;
 
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import reactivefeign.spring.config.ReactiveFeignClient;
 import reactor.core.publisher.Mono;
 
 //for this client we need to specify eureka application manually in application.yml
-@ReactiveFeignClient(name = "greeting-other")
+//temporary workaround due to issue https://github.com/spring-cloud/spring-cloud-commons/issues/951
+//@ReactiveFeignClient(name = "greeting-other")
+@ReactiveFeignClient(qualifier = "greeting-other-reactive", name = "${greeting-other.service.name:greeting-other}")
 public interface GreetingReactiveWOtherName {
 
     @GetMapping("/greeting")
